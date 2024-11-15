0d184d43683eec9c4ef78549fa7d711a90e80313
Vishal Singh <kvishalin17@gmail.com>
2024-11-15 22:42:14 +0530

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import './login.css'
 
-function Login() {
-    const [count, setCount] = useState(0)
-
+function Login() 
     return (
         <>
             <div>
